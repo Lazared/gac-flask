0f1d507f10124b0f8697afad1dc921e649af730c
Lazared <eduard.enl@gmail.com>
2020-10-21 09:31:00 +0100

    Removed some code

--- script.py
+++ script.py
@@ -1,19 +1,4 @@
-import math
-import os
-import sys
-
 import requests
 
-# print(sys.version)
-print(sys.executable)
-
-
-def greet(who_to_greet):
-    greeting = "Hello, {}".format(who_to_greet)
-    return greeting
-
-
-# print(greet("World"))
-# print(greet("Eduard"))
 r = requests.get("https://anthropologistics.com")
 print(r.status_code)
